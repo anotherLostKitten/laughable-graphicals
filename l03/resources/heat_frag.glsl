6fdd0df7fd4bcf64c3a38073e24f5c1adb8342ef
anotherLostKitten <eggismagicchickensquared@gmail.com>
2022-11-11 02:40:49 -0500

    yoo glsl-mode.el

--- l03/resources/heat_frag.glsl
+++ l03/resources/heat_frag.glsl
@@ -14,25 +14,24 @@ uniform float materialShininess;
 
 void main(void) {
 	
-	vec3 v = normalize(-camSpacePosition);
-	vec3 n = normalize(camSpaceNormal);
-	vec3 l = normalize(lightCamSpacePosition - camSpacePosition);
+  vec3 v = normalize(-camSpacePosition);
+  vec3 n = normalize(camSpaceNormal);
+  vec3 l = normalize(lightCamSpacePosition - camSpacePosition);
 
-	// TODO: 4, 7, 11 Implement your GLSL per fragement lighting, heat colouring, and distance stripes here!
+  // TODO: 7, 11 Implement your GLSL heat colouring, and distance stripes here!
 
-	float diffuse=max(dot(n,v),0);
+  float diffuse=max(dot(n,v),0);
+  float specular=0.0;
+  if(diffuse>0.0){
 	vec3 hv=normalize(l+v);
-	float specular=max(0,dot(n,hv));
-	if(diffuse==0.0){
-		specular=0.0;
-	}else{
-		specular=pow(specular,materialShininess);
-	}
-	vec3 diffLight=(n.xyz*0.3+vec3(0.7, 0.7,0.7))*diffuse;
-	vec3 specLight=lightColor*specular;
+	specular=max(0,dot(n,hv));
+	specular=pow(specular,materialShininess);
+  }
+  vec3 diffLight=(n.xyz*0.3+vec3(0.7, 0.7,0.7))*diffuse;
+  vec3 specLight=lightColor*specular;
 
-	out_fragColor=clamp(vec4(diffLight+specLight,1),0,1);
-	// can use this to initially visualize the normal	
-    // out_fragColor =  clamp( vec4( n.xyz * 0.5 + vec3( 0.5, 0.5,0.5 ), 0.5 + utv + phiv ), 0, 1 );
-	// out_fragColor =  clamp( vec4( n.xyz * 0.5 + vec3( 0.5, 0.5,0.5 ), 1), 0, 1 );
+  out_fragColor=clamp(vec4(diffLight+specLight,1),0,1);
+  // can use this to initially visualize the normal
+  // out_fragColor =  clamp( vec4( n.xyz * 0.5 + vec3( 0.5, 0.5,0.5 ), 0.5 + utv + phiv ), 0, 1 );
+  // out_fragColor =  clamp( vec4( n.xyz * 0.5 + vec3( 0.5, 0.5,0.5 ), 1), 0, 1 );
 }
